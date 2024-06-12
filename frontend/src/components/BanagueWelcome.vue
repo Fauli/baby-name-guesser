ec921cffb3b55a7dce14a1be09feb4e41e6de896
fauli <f@sbebe.ch>
2024-06-12 22:46:10 +0200

    adding details and validation

--- frontend/src/components/BanagueWelcome.vue
+++ frontend/src/components/BanagueWelcome.vue
@@ -7,30 +7,31 @@
   </header>
   <br />
   <section>
-        <p>Welcome to Our Baby Name Guessing Game!!! 👶</p>
+        <p>Welcome to our Baby Name Guessing Game!!! 👶</p>
     </section>
     <br />
     <section>
-        <p>Exciting times await as we prepare to welcome our newest family member! <br />
+        <p>Exciting times await as we prepare to welcome our newest family member! 🐣<br />
           But before the big reveal, we invite you to join in a fun and unique challenge: <span style="font-weight: bold;">Guess our baby’s name!</span><br />
           Participate, have fun, and contribute to our little one's future all at once. 💙</p>
     </section>
     <br />
     <section>
-        <h2>How It Works:</h2>
+        <h2>How It Works</h2>
         <ul>
             <li>We’ve shortlisted some beautiful names for our baby. Your challenge is to guess the one we’ve chosen as <span style="font-weight: bold;">first name.</span></li>
             <li>Place your votes — each guess is CHF 10, and you can cast as many guesses as you like to increase your chances of winning, but only once... so choose wisely!</li>
             <li>Every single Franken raised will go into a fund for our baby’s future, making each guess a gift to our child.</li>
-            <li>Get it right, and you’re in for a treat — a delicious three-course meal prepared by us! The date of the meal will be arranged after her birth.</li>
+            <li>Get it right, and you’re in for a treat — a delicious three-course meal 🍲 prepared by us! The date of the meal will be arranged after her birth.</li>
         </ul>
     </section>
     <br />
     <section>
-        <h2>Important Rules:</h2>
+        <h2>Important Rules</h2>
         <ul>
-            <li>Make your choice carefully; once your vote is cast and the payment is made through Twint, PayPal, or Revolut, there's no changing it.</li>
+            <li>Make your choice carefully. Once your vote is cast and the payment is made through Twint, PayPal, or Revolut, there's no changing it.</li>
             <li>Once you have voted, you will be able to see the overall top 3 guessed names.</li>
+            <li>We already made the final decision on the name - so the game will not inflience our choice 😁</li>
         </ul>
     </section>
     <br />
